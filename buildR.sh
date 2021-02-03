71ac4de1f7b1eb8c93987308e681e41717ef5166
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-02-03 09:18:32 -0600

    Revert "Use odr_violation=0 when building, remove when running for checks"
    This reverts commit 6cb2df2deccd4ef1dc192d78189e7c17a3e699de.

--- buildR.sh
+++ buildR.sh
@@ -23,7 +23,7 @@
 
 # Environment variables from http://www.stats.ox.ac.uk/pub/bdr/memtests/README.txt
 #export ASAN_OPTIONS='detect_leaks=0:detect_odr_violation=0'
-export ASAN_OPTIONS='detect_leaks=0:detect_odr_violation=0'
+export ASAN_OPTIONS='detect_leaks=0:detect_odr_violation=1'
 export UBSAN_OPTIONS='print_stacktrace=1'
 export RJAVA_JVM_STACK_WORKAROUND=0
 export RGL_USE_NULL=true
@@ -55,13 +55,13 @@ mkdir -p /usr/local/${dirname}/
 
 cd /tmp/r-source
 
-echo 'CXX="g++ -fsanitize=address,undefined,bounds-strict -fno-omit-frame-pointer"
-CFLAGS="-g -O2 -Wall -pedantic -mtune=native -fsanitize=address"
-DEFS=-DSWITCH_TO_REFCNT
-FFLAGS="-g -O2 -mtune=native"
-CXXFLAGS="-g -O2 -Wall -pedantic -mtune=native"
-MAIN_LDFLAGS="-fsanitize=address,undefined -pthread"
-' >> /tmp/r-source/config.site
+# echo 'CXX="g++ -fsanitize=address,undefined,bounds-strict -fno-omit-frame-pointer"
+# CFLAGS="-g -O0 -Wall -pedantic -mtune=native -fsanitize=address"
+# DEFS=-DSWITCH_TO_REFCNT
+# FFLAGS="-g -O0 -mtune=native"
+# CXXFLAGS="-g -O0 -Wall -pedantic -mtune=native"
+# MAIN_LDFLAGS="-fsanitize=address,undefined -pthread"
+# ' >> /tmp/r-source/config.site
 
 rm -rf ~/.R
 mkdir ~/.R
@@ -70,11 +70,16 @@ F77 = gfortran -fsanitize=address
 FC = gfortran -fsanitize=address
 ' > ~/.R/Makevars
 
-export ASAN_OPTIONS='detect_leaks=0'
-export UBSAN_OPTIONS='print_stacktrace=1'
-export RJAVA_JVM_STACK_WORKAROUND='0'
-export RGL_USE_NULL='true'
-export R_DONT_USE_TK='true'
+export CC="gcc -std=gnu99 -fsanitize=address,undefined -fno-omit-frame-pointer"
+export F77="gfortran -fsanitize=address"
+export FC="gfortran -fsanitize=address"
+
+export CXX="g++ -fsanitize=address,undefined,bounds-strict -fno-omit-frame-pointer"
+export CFLAGS="-g -O0 -Wall -pedantic -fsanitize=address"
+export DEFS=-DSWITCH_TO_REFCNT
+export FFLAGS="-g -O0"
+export CXXFLAGS="-g -O0 -Wall -pedantic"
+export MAIN_LDFLAGS="-fsanitize=address,undefined -pthread"
 
 export configure_flags="--without-recommended-packages --disable-openmp"
 
